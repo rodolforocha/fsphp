096abe9e36db90d49abc89757c8461de718f3aae
Rodolfo Rocha <rodolfo.rocha@ccc.ufcg.edu.br>
2022-04-15 20:30:05 -0300

    Abril

--- 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -7,8 +7,33 @@ fullStackPHPClassName("03.04 - Manipulação de objetos");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$arrProfile = [
+    "name" => "Rodolfo",
+    "company" => "Upinside",
+    "mail" => "rodolfoalr@gmail.com"
+];
 
-/**
+$objProfile = (object)$arrProfile;
+
+echo "<p>{$arrProfile["name"]} trabalha na {$arrProfile["company"]}</p>";
+echo "<p>{$objProfile->name} trabalha na {$objProfile->company}</p>";
+
+$ceo = $objProfile;
+unset($ceo->company);
+var_dump($ceo);
+
+$company = new StdClass();
+$company->company = "Upinside";
+$company->ceo = $ceo;
+$company->manager = new StdClass();
+$company->manager->name = "Dofas";
+$company->manager->mail = "truedofas@hotmail.com";
+
+var_dump($company);
+
+/**#
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
+var_dump(["class" => get_class($company)]);
